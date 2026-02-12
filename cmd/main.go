f99c0f8660f0ad3050d5025d5a361be58bfd58a2
ofcorp <director@demrev.biz>
2026-02-12 22:57:50 +0300

    2-logger dz1

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"ps-go-fiber/blog/config"
 	"ps-go-fiber/blog/internal/pages"
+	"ps-go-fiber/blog/pkg/logger"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/recover"
+	slogfiber "github.com/samber/slog-fiber"
 )
 
 func main() {
 	config.Init()
-	// dbConf := config.NewDatabaseConfig()
+	config.NewDatabaseConfig()
+
+	config.NewDatabaseConfig()
+	logConfig := config.NewLogConfig()
+	customLogger := logger.NewLogger(logConfig)
+
 	app := fiber.New()
+
+	app.Use(slogfiber.New(customLogger))
+
+	app.Use(recover.New())
 
 	pages.NewHandler(app)
 
--- config/env.go
+++ config/env.go
@@ -43,11 +43,23 @@ func getBool(key string, defaultValue bool) bool {
 }
 
 type DatabaseConfig struct {
-	url string
+	Url string
 }
 
 func NewDatabaseConfig() *DatabaseConfig {
 	return &DatabaseConfig{
-		url: getString("DATABASE_URL", ""),
+		Url: getString("DATABASE_URL", ""),
+	}
+}
+
+type LogConfig struct {
+	Level  int
+	Format string
+}
+
+func NewLogConfig() *LogConfig {
+	return &LogConfig{
+		Level:  getInt("LOG_LEVEL", 0),
+		Format: getString("LOG_FORMAT", "json"),
 	}
 }
